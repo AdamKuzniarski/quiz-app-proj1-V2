b8e2957386924075a5b6a3d04bf059ae80ec3f06
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-06-20 14:13:49 +0200

    changed js functions

--- js/script.js
+++ js/script.js
@@ -37,36 +37,44 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
-  console.log(data);
+  
 });
 
 /* new Card */
 const mainSection = document.querySelector('[data-js="main-element"]');
-const newCard = document.createElement("div");
 
-newCard.innerHTML += `
-      <article class="card border-btm">
-          <div class="question">
-            <button class="icon-button" data-js="bookmark1">
-              <i class="fa-solid fa-bookmark question__bookmark"></i>
-            </button>
-            <h2>
-              Question: ${data.question}?
-            </h2>
-          </div>
-          <div class="answer">
-            <button class="answer__btn" data-js="show-answer1-button">
-              Show the answer
-            </button>
-            <h3 class="hide" data-js="show-answer1">${data.answer}</h3>
-            <div class="tags-container">
-              <p class="tag">#${data.tags}</p>
-              
-            </div>
-          </div>
-        </article>
-`;
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  
 
-if (mainSection) {
-  mainSection.appendChild(newCard);
-}
+  const newCard = document.createElement("div");
+  newCard.innerHTML += `
+    <article class="card border-btm">
+      <div class="question">
+        <button class="icon-button" data-js="bookmark1">
+          <i class="fa-solid fa-bookmark question__bookmark"></i>
+        </button>
+        <h2>
+          Question: ${data.question}?
+        </h2>
+      </div>
+      <div class="answer">
+        <button class="answer__btn" data-js="show-answer1-button">
+          Show the answer
+        </button>
+        <h3 class="hide" data-js="show-answer1">${data.answer}</h3>
+        <div class="tags-container">
+          <p class="tag">#${data.tags}</p>
+        </div>
+      </div>
+    </article>
+  `;
+
+  if (mainSection) {
+    mainSection.appendChild(newCard);
+  }
+form.reset()
+
+});
