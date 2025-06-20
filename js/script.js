a1466f65f53fc9ff54c667ce0a053cdf3f17ea3a
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-06-20 11:47:17 +0200

    add create button in HTML + css

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 const bookmarkButton = document.querySelector("[data-js=bookmark1]");
+const textArea1 = document.querySelector("[data-js=textarea1-counter]");
+const textArea2 = document.querySelector("[data-js=textarea2-counter]");
+
+textArea1.addEventListener("input", updateCharCount);
+
 
 bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("bookmark--mark");
@@ -13,3 +18,13 @@ showAnswerButton.addEventListener("click", () => {
   answer1H3Element.classList.remove("hide");
   showAnswerButton.classList.add("hide");
 });
+
+function updateCharCount() {
+  const textarea = document.querySelector('[data-js="textarea1-counter"]');
+  const maxLength = textarea.getAttribute("maxlength");
+  const currentLength = textarea.value.length;
+  document.getElementById("char-count").textContent = `${
+    maxLength - currentLength
+  } characters left`;
+}
+
